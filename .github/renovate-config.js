d011770e792329fa78b114b14360fe41d4a0939b
Francesco Astegiano <francesco.astegiano@gmail.com>
2026-01-31 02:29:10 +0100

    [renovate] set-up workflow

---
+++ .github/renovate-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  branchPrefix: 'renovate/',
+  username: 'renovate',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  onboarding: false,
+  platform: 'github',
+  repositories: [
+    'k8s-home-lab/helm-charts'
+  ]
+};
